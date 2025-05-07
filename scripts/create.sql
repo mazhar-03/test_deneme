9cfff1901eb9153a0f7d42892779964278818916
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-05-07 13:32:12 +0200

    add db values

---
+++ scripts/create.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE Product(
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    NAME NVARCHAR(100) NOT NULL,
+    PRICE FLOAT(3) NOT NULL
+)
+
+CREATE TABLE Category(
+                         ID INT PRIMARY KEY IDENTITY(1,1),
+                         NAME NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Product_Category (
+                                  Product_Id INT,
+                                  Category_Id INT,
+                                  PRIMARY KEY (Product_Id, Category_Id),
+                                  FOREIGN KEY (Product_Id) REFERENCES Product(Id) ON DELETE CASCADE,
+                                  FOREIGN KEY (Category_Id) REFERENCES Category(Id) ON DELETE CASCADE
+);
---
+++ scripts/insert.sql
@@ -0,0 +1,27 @@
+﻿INSERT INTO Product (Name, Price)
+VALUES ('Laptop', 1200.00),
+       ('Smartphone', 800.00),
+       ('Tablet', 450.00),
+       ('Smartwatch', 250.00);
+
+INSERT INTO Category (Name)
+VALUES ('Electronics'),
+       ('Computers'),
+       ('Mobile Devices'),
+       ('Wearables');
+
+INSERT INTO Product_Category (Product_Id, Category_Id)
+VALUES (1, 1), 
+       (1, 2); 
+
+INSERT INTO Product_Category (Product_Id, Category_Id)
+VALUES (2, 1),  
+       (2, 3); 
+
+INSERT INTO Product_Category (Product_Id, Category_Id)
+VALUES (3, 1), 
+       (3, 3); 
+
+INSERT INTO Product_Category (Product_Id, Category_Id)
+VALUES (4, 1), 
+       (4, 4);  
